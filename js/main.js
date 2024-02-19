b8dc9e6d647710c1d5e5d004c559a4e3a5a2d67e
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-19 20:15:54 +0000

    Added  input event listener to photo url input that changes img src attribute when  user pastes link. #1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
+'use strict';
 /* global data */
-
 const $photoURLInput = document.querySelector('#url');
 const $img = document.querySelector('.img');
-
 if (!$photoURLInput) throw new Error("There's no photo url input element");
 if (!$img) throw new Error("There's no img element");
-
-$photoURLInput.addEventListener('input', (event: Event): void => {
-  const eventTarget = event.target as HTMLInputElement;
+$photoURLInput.addEventListener('input', (event) => {
+  const eventTarget = event.target;
   $img.setAttribute('src', eventTarget.value);
 });
-
-console.log(data);

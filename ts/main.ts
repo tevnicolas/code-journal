28cb09a9d21a33528bf7dc753477ff77d4b5efd0
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-19 20:36:03 +0000

    Added submit event listener, prevented default, created new interface for formelements, created new formelements var, stored forms input values in new object. #1

--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,32 @@
 /* global data */
+interface FormElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  url: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
 
 const $photoURLInput = document.querySelector('#url');
 const $img = document.querySelector('.img');
+const $form = document.querySelector('.main-form') as HTMLFormElement;
 
 if (!$photoURLInput) throw new Error("There's no photo url input element");
 if (!$img) throw new Error("There's no img element");
+if (!$form) throw new Error("There's no form element");
 
 $photoURLInput.addEventListener('input', (event: Event): void => {
   const eventTarget = event.target as HTMLInputElement;
   $img.setAttribute('src', eventTarget.value);
+});
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const $formElements = $form.elements as FormElements;
+  const entriesObject = {
+    title: $formElements.title.value,
+    url: $formElements.url.value,
+    notes: $formElements.notes.value,
+  };
+  console.log(entriesObject);
 });
 
 console.log(data);
